25ed1ff5f7d4aff69394cbb9f9d36e29e76aeb14
Wagner Cupertino Lemos <wagner.lemos@ses.ba>
2021-05-17 14:43:47 -0300

    Criado component de publicar

---
+++ src/components/social/PublicarConteudoVue.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="row">
+      <grid-vue class="input-field" tamanho="12">
+        <textarea v-model="conteudo" id="conteudoId" class="materialize-textarea"></textarea>
+        <label for="conteudoId">O que está acontecendo?</label>
+      </grid-vue>
+      <p>
+        <grid-vue v-if="conteudo" class="btn waves-effect waves-light" tamanho="2 offset-s10">Publicar</grid-vue>
+      </p>
+    </div>
+</template>
+
+<script>
+import GridVue from "@/components/layouts/GridVue";
+
+export default {
+  name: "PublicarConteudoVue",
+  props: ["perfil", "nome", "data"],
+  data() {
+    return {
+      conteudo:''
+    };
+  },
+  components: {
+    GridVue,
+  },
+};
+</script>
+
+<style>
+</style>
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,22 +1,27 @@
 <template>
-  <card-conteudo-vue
-    perfil="https://materializecss.com/images/yuna.jpg"
-    nome="Maria Silva"
-    data="17/05/2021 11:30"
-  >
-    <card-detalhe-vue
-      img="https://materializecss.com/images/sample-1.jpg"
-      titulo=""
-      txt="I am a very simple card. I am good at containing small bits of
+  <span>
+    <publicar-conteudo-vue />
+
+    <card-conteudo-vue
+      perfil="https://materializecss.com/images/yuna.jpg"
+      nome="Maria Silva"
+      data="17/05/2021 11:30"
+    >
+      <card-detalhe-vue
+        img="https://materializecss.com/images/sample-1.jpg"
+        titulo=""
+        txt="I am a very simple card. I am good at containing small bits of
         information. I am convenient because I require little markup to use
         effectively."
-    />
-  </card-conteudo-vue>
+      />
+    </card-conteudo-vue>
+  </span>
 </template>
 
 <script>
 import CardConteudoVue from "@/components/social/CardConteudoVue";
 import CardDetalheVue from "@/components/social/CardDetalheVue";
+import PublicarConteudoVue from "@/components/social/PublicarConteudoVue";
 export default {
   name: "Home",
   data() {
@@ -25,6 +30,7 @@ export default {
   components: {
     CardConteudoVue,
     CardDetalheVue,
+    PublicarConteudoVue
   },
 };
 </script>
