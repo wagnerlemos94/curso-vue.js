96486539003f717bf91f6055596c9abbebd414f7
Wagner Cupertino Lemos <wagner.lemos@ses.ba>
2021-05-17 11:58:54 -0300

    Ajuste no componet CardConteudoVue

--- src/components/social/CardConteudoVue.vue
+++ src/components/social/CardConteudoVue.vue
@@ -3,30 +3,15 @@
     <div class="card-content">
       <div class="row valign-wrapper">
         <grid-vue tamanho="1">
-          <img
-            src="https://materializecss.com/images/yuna.jpg"
-            alt=""
-            class="circle responsive-img"
-          />
-          <!-- notice the "circle" class -->
+          <img :src="perfil" :alt="nome" class="circle responsive-img">
         </grid-vue>
         <grid-vue tamanho="11">
           <span class="black-text">
-            <strong>Maria Silva</strong> - <small>17/05/2021 12:30</small>
+            <strong>{{ nome }}</strong> - <small>{{ data }}</small>
           </span>
         </grid-vue>
       </div>
-      <div class="card-image">
-        <img src="https://materializecss.com/images/sample-1.jpg" />
-        <span class="card-title">Card Title</span>
-      </div>
-      <div class="card-content">
-        <p>
-          I am a very simple card. I am good at containing small bits of
-          information. I am convenient because I require little markup to use
-          effectively.
-        </p>
-      </div>
+      <slot />
     </div>
     <div class="card-action">
       <p>
@@ -42,7 +27,7 @@ import GridVue from "@/components/layouts/GridVue";
 
 export default {
   name: "CardConteudoVue",
-  props: [],
+  props: ["perfil", "nome", "data"],
   data() {
     return {};
   },
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,20 +1,33 @@
 <template>
-  <card-conteudo-vue>
-    
-    
+  <card-conteudo-vue
+    perfil="https://materializecss.com/images/yuna.jpg"
+    nome="Maria Silva"
+    data="17/05/2021 11:30"
+  >
+    <div class="card-image">
+      <img src="https://materializecss.com/images/sample-1.jpg" />
+      <span class="card-title">Card Title</span>
+    </div>
+    <div class="card-content">
+      <p>
+        I am a very simple card. I am good at containing small bits of
+        information. I am convenient because I require little markup to use
+        effectively.
+      </p>
+    </div>
   </card-conteudo-vue>
 </template>
 
 <script>
-import CardConteudoVue from "@/components/social/CardConteudoVue"
+import CardConteudoVue from "@/components/social/CardConteudoVue";
 export default {
   name: "Home",
   data() {
     return {};
   },
-  components:{
-    CardConteudoVue
-  }
+  components: {
+    CardConteudoVue,
+  },
 };
 </script>
 
