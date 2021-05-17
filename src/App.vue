0bab61bf5ddc74cde9baabc21428e465f7310dce
Wagner Cupertino Lemos <wagner.lemos@ses.ba>
2021-05-17 10:31:54 -0300

    Criado componet footer

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,34 @@
 <template>
-  <div>
-    <nav-bar logo="Social" url="#" cor="green darken-1"/> 
-    <router-view />
-  </div>
+  <span>
+    <header>
+      <nav-bar logo="Social" url="#" cor="green darken-1" />
+    </header>
+
+    <main>
+      <div class="container">
+        <router-view />
+      </div>
+    </main>
+
+    <footer-vue cor="green darken-1" logo="Social" descricao="Teste de descricão" ano="2018">
+      <li><a class="grey-text text-lighten-3" href="#!">Home</a></li>
+      <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+      <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+      <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>  
+    </footer-vue>
+  </span>
 </template>
 
 <script>
-import NavBar from '@/components/layouts/NavBar'
+import NavBar from "@/components/layouts/NavBar";
+import FooterVue from "@/components/layouts/FooterVue";
 
 export default {
   name: "App",
-  components:{
-    NavBar
-  }
+  components: {
+    NavBar,
+    FooterVue    
+  },
 };
 </script>
 
