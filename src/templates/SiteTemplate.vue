3e0d762e68a1a69ba8821f44e12ac93d4363f317
Wagner Cupertino Lemos <wagner.lemos@ses.ba>
2021-05-17 16:40:27 -0300

    Otimizando Rotas

--- src/templates/SiteTemplate.vue
+++ src/templates/SiteTemplate.vue
@@ -1,9 +1,9 @@
 <template>
   <span>
     <header>
-      <nav-bar logo="Social" url="#/" cor="green darken-1">
-        <li><a href="#/">Home</a></li>
-        <li><a href="#/login">Entrar</a></li>
+      <nav-bar logo="Social" url="/" cor="green darken-1">
+        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/login">Entrar</router-link></li>
       </nav-bar>
     </header>
 
